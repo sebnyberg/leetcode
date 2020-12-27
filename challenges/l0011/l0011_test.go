3c8ec6207fb04fbeb1e5f77e6a105217baa6d366
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2020-12-27 20:51:06 +0100

    stop work on l11

---
+++ challenges/l0011/l0011_test.go
@@ -0,0 +1,20 @@
+package l0011_test
+
+// func Test_maxArea(t *testing.T) {
+// 	for _, tc := range []struct {
+// 		in   []int
+// 		want int
+// 	}{
+// 		{[]int{1, 1}, 1},
+// 		{[]int{4, 3, 2, 1, 4}, 16},
+// 		{[]int{1, 2, 1}, 2},
+// 	} {
+// 		t.Run(fmt.Sprintf("%+v", tc.in), func(t *testing.T) {
+// 			require.Equal(t, tc.want, maxArea(tc.in))
+// 		})
+// 	}
+// }
+
+// func maxArea(height []int) int {
+// 	return 0
+// }
