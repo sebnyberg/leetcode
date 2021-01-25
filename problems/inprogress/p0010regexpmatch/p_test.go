ecb085e132cb792b6c3a22bad7a4772d0edc2487
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2021-01-25 21:55:26 +0100

    simpler solution

--- problems/inprogress/p0010regexpmatch/p_test.go
+++ problems/inprogress/p0010regexpmatch/p_test.go
@@ -1,59 +1,47 @@
 package p0010regexpmatch
 
-// func Test_l0010(t *testing.T) {
-// 	tcs := []struct {
-// 		in   string
-// 		in2  string
-// 		want bool
-// 	}{
-// 		// {"a", "ab*a", false},
-// 		{"a", "ab*", true},
-// 		// {"aa", "a", false},
-// 		// {"aa", "a*", true},
-// 		// {"aaa", "a*a", true},
-// 		// {"ab", ".*", true},
-// 		// {"ab", ".*c", false},
-// 		// {"aab", "c*a*b", true},
-// 		// {"mississippi", "mis*is*p*.", false},
-// 	}
-// 	for _, tc := range tcs {
-// 		t.Run(fmt.Sprintf("%v/%v", tc.in, tc.in2), func(t *testing.T) {
-// 			require.Equal(t, tc.want, isMatch(tc.in, tc.in2))
-// 		})
-// 	}
-// }
+import (
+	"fmt"
+	"testing"
 
-// func isMatch(s string, p string) bool {
-// 	sx := 0
-// 	px := 0
-// 	for sx < len(p) || px < len(s) {
-// 		if px < len(p) {
-// 			c := p[px]
-// 			switch c {
-// 			case '.':
-// 				if sx < len(s) {
-// 					px++
-// 					sx++
-// 					continue
-// 				}
-// 			case '*':
-// 				// Try to match at sx, sx+1 and so on
-// 				for ; sx < len(s); sx++ {
-// 					if isMatch(p[px+1:], s[sx:]) {
-// 						return true
-// 					}
-// 				}
-// 			default: // Regular character
-// 				if sx < len(s) && s[sx] == c {
-// 					px++
-// 					sx++
-// 					continue
-// 				}
-// 			}
-// 		}
-// 		// Mismatch
-// 		return false
-// 	}
-// 	// The entire pattern and string has been matched
-// 	return true
-// }
+	"github.com/stretchr/testify/require"
+)
+
+func Test_isMatch(t *testing.T) {
+	tcs := []struct {
+		s    string
+		p    string
+		want bool
+	}{
+		{"a", "ab*a", false},
+		{"a", "ab*", true},
+		{"aa", "a", false},
+		{"aa", "a*", true},
+		{"aaa", "a*a", true},
+		{"ab", ".*", true},
+		{"ab", ".*c", false},
+		{"aab", "c*a*b", true},
+		{"mississippi", "mis*is*p*.", false},
+		{"mississippi", "mis*is*ip*.", true},
+		{"aaa", "ab*ac*a", true},
+		{"aasdfasdfasdfasdfas", "aasdf.*asdf.*asdf.*asdf.*s", true},
+		{"caccccaccbabbcb", "c*c*b*a*.*c*.a*a*a*", true},
+	}
+	for _, tc := range tcs {
+		t.Run(fmt.Sprintf("%v/%v", tc.s, tc.p), func(t *testing.T) {
+			require.Equal(t, tc.want, isMatch(tc.s, tc.p))
+		})
+	}
+}
+
+func isMatch(s string, p string) bool {
+	if len(p) == 0 {
+		return len(s) == 0
+	}
+
+	match := len(s) > 0 && (s[0] == p[0] || p[0] == '.')
+	if len(p) >= 2 && p[1] == '*' {
+		return isMatch(s, p[2:]) || (match && isMatch(s[1:], p))
+	}
+	return match && isMatch(s[1:], p[1:])
+}
