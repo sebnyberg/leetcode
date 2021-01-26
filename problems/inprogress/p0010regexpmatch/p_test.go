2f7de4bfb4a7b7c481d2a8c052b9ec7ee428e541
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2021-01-26 09:04:21 +0100

    Done with regexp match but want to understand DP solution

--- problems/inprogress/p0010regexpmatch/p_test.go
+++ problems/inprogress/p0010regexpmatch/p_test.go
@@ -13,19 +13,19 @@ func Test_isMatch(t *testing.T) {
 		p    string
 		want bool
 	}{
-		{"a", "ab*a", false},
-		{"a", "ab*", true},
-		{"aa", "a", false},
-		{"aa", "a*", true},
-		{"aaa", "a*a", true},
+		// {"a", "ab*a", false},
+		// {"a", "ab*", true},
+		// {"aa", "a", false},
+		// {"aa", "a*", true},
+		// {"aaa", "a*a", true},
 		{"ab", ".*", true},
-		{"ab", ".*c", false},
-		{"aab", "c*a*b", true},
-		{"mississippi", "mis*is*p*.", false},
-		{"mississippi", "mis*is*ip*.", true},
-		{"aaa", "ab*ac*a", true},
-		{"aasdfasdfasdfasdfas", "aasdf.*asdf.*asdf.*asdf.*s", true},
-		{"caccccaccbabbcb", "c*c*b*a*.*c*.a*a*a*", true},
+		// {"ab", ".*c", false},
+		// {"aab", "c*a*b", true},
+		// {"mississippi", "mis*is*p*.", false},
+		// {"mississippi", "mis*is*ip*.", true},
+		// {"aaa", "ab*ac*a", true},
+		// {"aasdfasdfasdfasdfas", "aasdf.*asdf.*asdf.*asdf.*s", true},
+		// {"caccccaccbabbcb", "c*c*b*a*.*c*.a*a*a*", true},
 	}
 	for _, tc := range tcs {
 		t.Run(fmt.Sprintf("%v/%v", tc.s, tc.p), func(t *testing.T) {
@@ -35,13 +35,32 @@ func Test_isMatch(t *testing.T) {
 }
 
 func isMatch(s string, p string) bool {
-	if len(p) == 0 {
-		return len(s) == 0
+	np := len(p)
+	ns := len(s)
+	dp := make([][]bool, ns+1)
+	for i := range dp {
+		dp[i] = make([]bool, np+1)
+	}
+	dp[0][0] = true
+	for i := 2; i <= np; i++ {
+		if p[i-1] == '*' {
+			dp[0][i] = dp[0][i-2]
+		}
 	}
 
-	match := len(s) > 0 && (s[0] == p[0] || p[0] == '.')
-	if len(p) >= 2 && p[1] == '*' {
-		return isMatch(s, p[2:]) || (match && isMatch(s[1:], p))
+	for i := 1; i <= ns; i++ {
+		for j := 1; j <= np; j++ {
+			if s[i-1] == p[j-1] || p[j-1] == '.' {
+				dp[i][j] = dp[i-1][j-1]
+			} else if p[j-1] == '*' {
+				if j > 1 {
+					dp[i][j] = dp[i][j-2]
+					if p[j-2] == s[i-1] || p[j-2] == '.' {
+						dp[i][j] = dp[i][j] || dp[i-1][j-2] || dp[i-1][j]
+					}
+				}
+			}
+		}
 	}
-	return match && isMatch(s[1:], p[1:])
+	return dp[len(s)][len(p)]
 }
