7031704f745c27eb08e44a0a44bb69d6e5a20e26
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2021-02-07 22:34:40 +0100

    before perf refactor

---
+++ problems/inprogress/p0057insertinterval/p_test.go
@@ -0,0 +1,58 @@
+package p0057insertinterval
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_insert(t *testing.T) {
+	for _, tc := range []struct {
+		intervals   [][]int
+		newInterval []int
+		want        [][]int
+	}{
+		{[][]int{{1, 5}}, []int{0, 3}, [][]int{{0, 5}}},
+		{[][]int{{1, 5}, {10, 11}}, []int{6, 7}, [][]int{{1, 5}, {6, 7}, {10, 11}}},
+		{[][]int{{1, 3}, {6, 9}}, []int{2, 5}, [][]int{{1, 5}, {6, 9}}},
+		{[][]int{{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}}, []int{4, 8}, [][]int{{1, 2}, {3, 10}, {12, 16}}},
+		{[][]int{}, []int{5, 7}, [][]int{{5, 7}}},
+		{[][]int{{1, 5}}, []int{2, 3}, [][]int{{1, 5}}},
+		{[][]int{{1, 5}}, []int{2, 7}, [][]int{{1, 7}}},
+	} {
+		t.Run(fmt.Sprintf("%+v/%+v", tc.intervals, tc.newInterval), func(t *testing.T) {
+			require.Equal(t, tc.want, insert(tc.intervals, tc.newInterval))
+		})
+	}
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func insert(intervals [][]int, newInterval []int) [][]int {
+	// res := make([][]int, 0, len(intervals)
+	var i int
+	n := len(intervals)
+	for ; i < len(intervals) && newInterval[0] > intervals[i][1]; i++ {
+	}
+	if i == n {
+		return append(intervals, newInterval)
+	}
+	if newInterval[0] < intervals[i][0] || newInterval[1] < intervals[i][0] {
+		intervals = append(intervals, []int{})
+		copy(intervals[i+1:], intervals[i:])
+		intervals[i] = newInterval
+	}
+
+	intervals[i][1] = max(intervals[i][1], newInterval[1])
+	for i < len(intervals)-1 && intervals[i+1][0] <= intervals[i][1] {
+		intervals[i][1] = max(intervals[i][1], intervals[i+1][1])
+		intervals = append(intervals[:i+1], intervals[i+2:]...)
+	}
+	return intervals
+}
