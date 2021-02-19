ba54089cbeb637cb3b849a72c4a055eea998223b
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2021-02-19 09:56:40 +0100

    more user-friendly text

--- done/p1200/p1249minremovemakevalidparen/p_test.go
+++ done/p1200/p1249minremovemakevalidparen/p_test.go
@@ -25,25 +25,30 @@ func Test_minRemoveToMakeValid(t *testing.T) {
 }
 
 func minRemoveToMakeValid(s string) string {
-	//
-	lparens := make([]int, 0)
-	skip := make(map[int]struct{})
+	// Create a map of indices to skip
+	// Indices to skip are either leading ')'s or trailing '('s
+	lparens := make([]int, 0)      // (stack) of left paren indices
+	skip := make(map[int]struct{}) // map of indices to skip
 	for i, ch := range s {
 		switch ch {
 		case '(':
-			lparens = append(lparens, i)
+			lparens = append(lparens, i) // add to stack
 		case ')':
 			if len(lparens) == 0 {
-				skip[i] = struct{}{}
+				skip[i] = struct{}{} // leading ')'
 			} else {
+				// Pop '(' from the stack
 				lparens = lparens[:len(lparens)-1]
 			}
 		}
 	}
+
+	// Add remaining '(' indices to the map of indices to skip
 	for _, pos := range lparens {
 		skip[pos] = struct{}{}
 	}
 
+	// Create resulting string
 	var res strings.Builder
 	for i, ch := range s {
 		if _, exists := skip[i]; exists {
