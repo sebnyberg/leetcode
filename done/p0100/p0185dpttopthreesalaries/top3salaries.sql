3318dfbbc124cdd79d4017effaf35436b42c28d2
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2021-03-01 00:13:51 +0100

    add comments to sql solution

--- done/p0100/p0185dpttopthreesalaries/top3salaries.sql
+++ done/p0100/p0185dpttopthreesalaries/top3salaries.sql
@@ -1,3 +1,17 @@
+/* Write your T-SQL query statement below */
+-- Below is a better solution:
+-- select a.Department, a.Employee , a.Salary 
+-- from (
+--     select d.Name as Department, 
+--     e.Name as Employee, 
+--     e.Salary, 
+--     DENSE_RANK() OVER(Partition by d.Id order by Salary desc) as rnk
+--     from Employee as e join Department as d 
+--     on e.DepartmentId = d.Id
+-- ) as a
+-- where a.rnk <=3
+
+-- Here was my attempt, roughly 20% slower than the above optimal
 with GroupedSalaries as (
     select DepartmentId, Salary
     from Employee e
