9f37cb5bd51b4a064b9d415b33a2e11fccbe32c1
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2021-03-02 22:56:38 +0100

    add another tortoise and hare solution

---
+++ done/p0200/p0287findduplicatenumber/p_test.go
@@ -0,0 +1,38 @@
+package p0287findduplicatenumber
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_findDuplicate(t *testing.T) {
+	for _, tc := range []struct {
+		nums []int
+		want int
+	}{
+		{[]int{1, 3, 4, 2, 2}, 2},
+		{[]int{3, 1, 3, 4, 2}, 3},
+		{[]int{1, 1}, 1},
+		{[]int{1, 1, 2}, 1},
+	} {
+		t.Run(fmt.Sprintf("%+v", tc.nums), func(t *testing.T) {
+			require.Equal(t, tc.want, findDuplicate(tc.nums))
+		})
+	}
+}
+
+func findDuplicate(nums []int) int {
+	slow, fast := nums[nums[0]], nums[nums[nums[0]]]
+	for slow != fast {
+		slow = nums[slow]
+		fast = nums[nums[fast]]
+	}
+	slow = nums[0]
+	for slow != fast {
+		slow = nums[slow]
+		fast = nums[fast]
+	}
+	return slow
+}
