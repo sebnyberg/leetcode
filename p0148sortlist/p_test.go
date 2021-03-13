affad2d5eccfa8ab6383049a117ca26e0b34fc8a
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2021-03-13 12:21:49 +0100

    some new stuff

---
+++ p0148sortlist/p_test.go
@@ -0,0 +1,23 @@
+package p0148sortlist
+
+// import "testing"
+
+// func Test_sortList(t *testing.T) {
+// 	n := &ListNode{Val: 4}
+// 	cur := n
+// 	for _, v := range []int{4, 3, 1, 3} {
+// 		cur.Next = &ListNode{Val: v}
+// 		cur = cur.Next
+// 	}
+// 	res := sortList(n)
+// 	_ = res
+// }
+
+// type ListNode struct {
+// 	Val  int
+// 	Next *ListNode
+// }
+
+// func sortList(head *ListNode) *ListNode {
+// 	slow, fast := head, head.Next
+// }
---
+++ p0823binarytreewithfactors/p_test.go
@@ -0,0 +1,5 @@
+package p0823binarytreewithfactors
+
+// func numFactoredBinaryTrees(arr []int) int {
+
+// }
