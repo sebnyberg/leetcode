8d50fb5f98a09dd410d5813a5adf86f6fc683624
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2021-04-16 15:40:33 +0200

    daily

--- done/p1700/p1786numberofrestrictedpathsfromfirsttolastnode/p_test.go
+++ done/p1700/p1786numberofrestrictedpathsfromfirsttolastnode/p_test.go
@@ -1,6 +1,7 @@
 package p1786numberofrestrictedpathsfromfirsttolastnode
 
 import (
+	"container/heap"
 	"fmt"
 	"math"
 	"testing"
@@ -14,7 +15,7 @@ func Test_countRestrictedPaths(t *testing.T) {
 		edges [][]int
 		want  int
 	}{
-		// {5, [][]int{{1, 2, 3}, {1, 3, 3}, {2, 3, 1}, {1, 4, 2}, {5, 2, 2}, {3, 5, 1}, {5, 4, 10}}, 3},
+		{5, [][]int{{1, 2, 3}, {1, 3, 3}, {2, 3, 1}, {1, 4, 2}, {5, 2, 2}, {3, 5, 1}, {5, 4, 10}}, 3},
 		// {7, [][]int{{1, 5, 1}, {4, 1, 2}, {7, 3, 4}, {2, 5, 3}, {5, 6, 1}, {6, 7, 2}, {7, 5, 3}}, 3},
 	} {
 		t.Run(fmt.Sprintf("%v/%+v", tc.n, tc.edges), func(t *testing.T) {
@@ -23,10 +24,9 @@ func Test_countRestrictedPaths(t *testing.T) {
 	}
 }
 
-// TODO
 type weightedEdge struct {
 	weight int
-	to     int
+	node   int
 }
 
 func countRestrictedPaths(n int, edges [][]int) int {
@@ -37,47 +37,72 @@ func countRestrictedPaths(n int, edges [][]int) int {
 		adj[b] = append(adj[b], &weightedEdge{weight, a})
 	}
 
-	mem := make([]int, n+1)
-	for i := range mem {
-		mem[i] = -1
+	// Dijkstra (distance from n to a node)
+	dist := make([]int, n+1)
+	for i := range dist {
+		dist[i] = math.MaxInt32
 	}
-	for i := 1; i < n; i++ {
-		findShortestPath(mem, i, n, adj)
-	}
-
-	nrestricted := 0
-	for i := 1; i < n-1; i++ {
-		for j := i + 1; j < n; j++ {
-			if mem[i] > mem[j] {
-				nrestricted++
+	dist[n] = 0
+	h := WeightedEdgeHeap{&weightedEdge{0, n}}
+	heap.Init(&h)
+	for h.Len() > 0 {
+		edge := heap.Pop(&h).(*weightedEdge)
+		if edge.weight != dist[edge.node] { // not shortest path
+			continue
+		}
+		for _, near := range adj[edge.node] {
+			if d := dist[edge.node] + near.weight; d < dist[near.node] {
+				dist[near.node] = d
+				heap.Push(&h, &weightedEdge{dist[near.node], near.node})
 			}
 		}
 	}
 
-	return nrestricted
+	mem := make([]int, n+1)
+	for i := range mem {
+		mem[i] = -1
+	}
+
+	return findRestricted(mem, dist, adj, n, 1)
 }
 
-func findShortestPath(mem []int, i, n int, adjWeights [][]*weightedEdge) int {
+const mod = 1000000007
+
+func findRestricted(mem []int, dist []int, adj [][]*weightedEdge, n, i int) int {
 	if mem[i] != -1 {
 		return mem[i]
 	}
 	if i == n {
-		return 0
+		return 1
 	}
-	minSum := math.MaxInt32
-	for _, edge := range adjWeights[i] {
-		if edge.to <= i {
-			continue
+	res := 0
+	for _, near := range adj[i] {
+		if dist[near.node] > dist[i] {
+			res += findRestricted(mem, dist, adj, n, near.node)
+			res %= mod
 		}
-		minSum = min(minSum, edge.weight+findShortestPath(mem, edge.to, n, adjWeights))
 	}
-	mem[i] = minSum
-	return minSum
+	mem[i] = res
+	return res
 }
 
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
+type WeightedEdgeHeap []*weightedEdge
+
+func (h WeightedEdgeHeap) Len() int { return len(h) }
+func (h WeightedEdgeHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+	// h[i].idx = i
+	// h[j].idx = j
+}
+func (h WeightedEdgeHeap) Less(i, j int) bool {
+	return h[i].weight < h[i].weight
+}
+func (h *WeightedEdgeHeap) Push(x interface{}) {
+	*h = append(*h, x.(*weightedEdge))
+}
+func (h *WeightedEdgeHeap) Pop() interface{} {
+	n := len(*h)
+	it := (*h)[n-1]
+	*h = (*h)[:n-1]
+	return it
 }
