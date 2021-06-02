80c6f9c81d5a99e0363c6702b47a2c490efc4849
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2021-06-02 22:38:00 +0200

    finally solve balloon burst problem

--- done/p0300/p0312burstballoons/p_test.go
+++ done/p0300/p0312burstballoons/p_test.go
@@ -22,5 +22,45 @@ func Test_maxCoins(t *testing.T) {
 }
 
 func maxCoins(nums []int) int {
-	return 0
+	newNums := make([]int, 0, len(nums)+2)
+	newNums = append(newNums, 1)
+	for _, num := range nums {
+		if num > 0 {
+			newNums = append(newNums, num)
+		}
+	}
+	newNums = append(newNums, 1)
+	nums = newNums
+	n := len(nums)
+
+	// dp[i][j] = max coins from popping balloons between i and j
+	dp := make([][]int, n)
+	for i := range dp {
+		dp[i] = make([]int, n)
+	}
+
+	// For each round, pop balloons of width k
+	for k := 1; k < n; k++ {
+		for left := 0; left+k < n; left++ {
+			right := left + k
+			// Find the max score from [left,right]
+			// Assume that a balloon at position i is the last balloon to burst
+			// Since it is the last balloon, the max score from its left and right
+			// side is sure to be independently solvable.
+			for i := left + 1; i < right; i++ {
+				dp[left][right] = max(dp[left][right],
+					nums[left]*nums[i]*nums[right]+dp[left][i]+dp[i][right],
+				)
+			}
+		}
+	}
+	// For all other cases, the max amount is the max of popping either side
+	return dp[0][n-1]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
