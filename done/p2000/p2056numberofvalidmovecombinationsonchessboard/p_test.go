36e8692d9d76c4d0b4c0a55ac5a4dad61a6e736e
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2021-10-31 13:47:57 +0100

    Biweekly 64

--- done/p2000/p2056numberofvalidmovecombinationsonchessboard/p_test.go
+++ done/p2000/p2056numberofvalidmovecombinationsonchessboard/p_test.go
@@ -25,79 +25,61 @@ func Test_countCombinations(t *testing.T) {
 	}
 }
 
-var pieceDirs = map[string][][]int{
+var pieceDirs = map[string][][]int8{
 	"rook":   {{1, 0}, {-1, 0}, {0, 1}, {0, -1}},
 	"queen":  {{0, -1}, {-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}},
 	"bishop": {{1, -1}, {1, 1}, {-1, 1}, {-1, -1}},
 }
 
 func countCombinations(pieces []string, positions [][]int) int {
-	// Brute-force
 	n := len(pieces)
 	var f combFinder
-	var pos [4][2]int8
+	var initialPos [4][2]int8
 	for i := range positions {
-		pos[i][0] = int8(positions[i][0]) - 1
-		pos[i][1] = int8(positions[i][1]) - 1
+		initialPos[i][0] = int8(positions[i][0]) - 1
+		initialPos[i][1] = int8(positions[i][1]) - 1
 	}
 	f.combs = make(map[[4][2]int8]struct{})
-	f.pickDirs(0, n, pieces, make([][]int, n), &pos)
-	return len(f.combs)
+	f.pickDirs(0, n, pieces, make([][]int8, n), &initialPos)
+	return len(f.combs) + 1
 }
 
 type combFinder struct {
 	combs map[[4][2]int8]struct{}
 }
 
-func (f *combFinder) pickDirs(i, n int, pieces []string, dirs [][]int, pos *[4][2]int8) {
+func (f *combFinder) pickDirs(i, n int, pieces []string, dirs [][]int8, initialPos *[4][2]int8) {
 	if i == n {
-		canMove := (1 << n) - 1
-		f.findComb(*pos, dirs, canMove, n)
+		for movingMask := 1; movingMask < (1 << n); movingMask++ {
+			f.findComb(*initialPos, dirs, movingMask, n)
+		}
 		return
 	}
 	for _, dir := range pieceDirs[pieces[i]] {
 		dirs[i] = dir
-		f.pickDirs(i+1, n, pieces, dirs, pos)
+		f.pickDirs(i+1, n, pieces, dirs, initialPos)
 	}
 }
 
-func (f *combFinder) findComb(pos [4][2]int8, dirs [][]int, canMove int, n int) {
-	f.combs[pos] = struct{}{}
-	if canMove == 0 {
-		return
-	}
-	move := func(toMove int) {
-		// Move pieces
-		for i := 0; i < n; i++ {
-			if toMove&(1<<i) == 0 { // skip stopped pieces
-				continue
-			}
-			pos[i][0] += int8(dirs[i][0])
-			pos[i][1] += int8(dirs[i][1])
-			defer func(j int) {
-				pos[j][0] -= int8(dirs[j][0])
-				pos[j][1] -= int8(dirs[j][1])
-			}(i)
-			if pos[i][0] < 0 || pos[i][0] >= 8 || pos[i][1] < 0 || pos[i][1] >= 8 {
-				return
-			}
-		}
-		// Check that the number of unique positions is equal to the number of pieces
-		uniquePos := make(map[[2]int8]struct{})
-		for i := 0; i < n; i++ {
-			uniquePos[pos[i]] = struct{}{}
-		}
-		if len(uniquePos) != n {
+func (f *combFinder) findComb(pos [4][2]int8, dirs [][]int8, movingMask, n int) {
+	var seen [8][8]bool
+	for i := 0; i < n; i++ {
+		pos[i][0] += int8((movingMask>>i)&1) * dirs[i][0]
+		pos[i][1] += int8((movingMask>>i)&1) * dirs[i][1]
+		if pos[i][0] < 0 || pos[i][0] >= 8 || pos[i][1] < 0 || pos[i][1] >= 8 ||
+			seen[pos[i][0]][pos[i][1]] {
 			return
 		}
-		f.findComb(pos, dirs, toMove, n)
+		seen[pos[i][0]][pos[i][1]] = true
 	}
 
-	// For each possible decision to move / stop a piece, continue searching
-	for toMove := 1; toMove < (1 << n); toMove++ {
-		if toMove&canMove != toMove {
+	f.combs[pos] = struct{}{}
+
+	// For each possible decision to move / stop moving a piece, continue search
+	for nextMovingMask := 1; nextMovingMask < (1 << n); nextMovingMask++ {
+		if nextMovingMask&movingMask != nextMovingMask {
 			continue
 		}
-		move(toMove)
+		f.findComb(pos, dirs, nextMovingMask, n)
 	}
 }
