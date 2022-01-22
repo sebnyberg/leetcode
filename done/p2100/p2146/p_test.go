abd60d16a102cc85afefc57c08c89991f635d4fd
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2022-01-22 20:42:45 +0100

    Fix p2146

--- done/p2100/p2146/p_test.go
+++ done/p2100/p2146/p_test.go
@@ -16,6 +16,7 @@ func Test_highestRankedKItems(t *testing.T) {
 		k       int
 		want    [][]int
 	}{
+		{[][]int{{1, 0, 1}, {3, 5, 2}, {1, 0, 1}}, []int{2, 5}, []int{1, 1}, 9, [][]int{{1, 1}, {1, 2}, {1, 0}}},
 		{[][]int{{1, 2, 0, 1}, {1, 3, 0, 1}, {0, 2, 5, 1}}, []int{2, 5}, []int{0, 0}, 3, [][]int{{0, 1}, {1, 1}, {2, 1}}},
 		{[][]int{{1, 2, 0, 1}, {1, 3, 3, 1}, {0, 2, 5, 1}}, []int{2, 3}, []int{2, 3}, 2, [][]int{{2, 1}, {1, 2}}},
 		{[][]int{{1, 1, 1}, {0, 0, 1}, {2, 3, 4}}, []int{2, 3}, []int{0, 0}, 3, [][]int{{2, 1}, {2, 0}}},
@@ -34,50 +35,53 @@ func highestRankedKItems(grid [][]int, pricing []int, start []int, k int) [][]in
 		i, j int
 	}
 
-	var seen [10001][10001]bool
-
-	canVisit := func(p pos) bool {
-		return p.i >= 0 && p.i < m && p.j >= 0 && p.j < n &&
-			grid[p.i][p.j] != 0 && !seen[p.i][p.j]
-	}
-	wantsToBuy := func(p pos) bool {
-		price := grid[p.i][p.j]
-		return price >= pricing[0] && price <= pricing[1]
+	seen := make([][]bool, m)
+	for i := range seen {
+		seen[i] = make([]bool, n)
 	}
 
 	// BFS visits cur, adding positions to next
 	cur := []pos{{start[0], start[1]}}
+	seen[start[0]][start[1]] = true
 	next := []pos{}
 
-	res := make([][]int, 0, k)
-	for len(cur) > 0 {
+	type distAndPos struct {
+		dist, i, j int
+	}
+
+	res := make([]distAndPos, 0, k)
+	for dist := 0; len(cur) > 0 && len(res) < k; dist++ {
 		next := next[:0] // re-use previous slice (note swap at end of for loop)
 
-		// Cur is sorted by price, row, col
-		for _, p := range cur {
-			if wantsToBuy(p) {
-				res = append(res, []int{p.i, p.j})
-				k--
-				if k == 0 {
-					return res
-				}
+		// Visit each cell at the current distance
+		for _, cell := range cur {
+			if price := grid[cell.i][cell.j]; price >= pricing[0] && price <= pricing[1] {
+				res = append(res, distAndPos{
+					dist: dist,
+					i:    cell.i,
+					j:    cell.j,
+				})
 			}
 
-			// Visit adjacent neighbours
-			for _, nei := range []pos{
-				{p.i + 1, p.j}, {p.i - 1, p.j}, {p.i, p.j + 1}, {p.i, p.j - 1},
-			} {
-				if !canVisit(nei) {
+			// Visit adjacent cells
+			for _, d := range [][]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}} {
+				ii := cell.i + d[0]
+				jj := cell.j + d[1]
+				if ii < 0 || ii >= m || jj < 0 || jj >= n ||
+					grid[ii][jj] == 0 || seen[ii][jj] {
 					continue
 				}
-				seen[nei.i][nei.j] = true
-				next = append(next, nei)
+				seen[ii][jj] = true
+				next = append(next, pos{ii, jj})
 			}
 		}
+		cur, next = next, cur
+	}
 
-		// Sort alternatives
-		sort.Slice(next, func(i, j int) bool {
-			p1, p2 := next[i], next[j]
+	// Sort result
+	sort.Slice(res, func(i, j int) bool {
+		p1, p2 := res[i], res[j]
+		if p1.dist == p2.dist { // Distance
 			if grid[p1.i][p1.j] == grid[p2.i][p2.j] { // Price
 				if p1.i == p2.i { // Row
 					return p1.j < p2.j // Column
@@ -85,9 +89,20 @@ func highestRankedKItems(grid [][]int, pricing []int, start []int, k int) [][]in
 				return p1.i < p2.i
 			}
 			return grid[p1.i][p1.j] < grid[p2.i][p2.j]
-		})
+		}
+		return p1.dist < p2.dist
+	})
 
-		cur, next = next, cur
+	// Trim excess items if necessary
+	if len(res) > k {
+		res = res[:k]
 	}
-	return res
+
+	// Convert to result type
+	ret := make([][]int, len(res))
+	for i := range res {
+		ret[i] = []int{res[i].i, res[i].j}
+	}
+
+	return ret
 }
