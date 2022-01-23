64fa1a0c576122e3e06dbc29ced172fccdfe0691
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2022-01-23 18:27:40 +0100

    Finally solve p2123

--- done/p2100/p2123minimumoperationstoremovadjacentonesinamatrix/p_test.go
+++ done/p2100/p2123minimumoperationstoremovadjacentonesinamatrix/p_test.go
@@ -2,24 +2,65 @@ package p2123minimumoperationstoremovadjacentonesinamatrix
 
 import (
 	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
+func parseBigInput() [][]int {
+	// Parse big input
+	bigInputBytes, _ := os.ReadFile("biginput")
+	bigInputStrs := strings.Split(string(bigInputBytes), "\n")
+	bigInput := make([][]int, len(bigInputStrs))
+	for i, row := range bigInputStrs {
+		for _, numStr := range strings.Split(row[1:len(row)-1], ",") {
+			n, _ := strconv.Atoi(numStr)
+			bigInput[i] = append(bigInput[i], n)
+		}
+	}
+	return bigInput
+}
+
+var res int
+
+func BenchmarkMinimumOperations(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		inp := parseBigInput()
+		b.StartTimer()
+		res = minimumOperations(inp)
+	}
+}
+
 func Test_minimumOperations(t *testing.T) {
+	// Parse big input
+	bigInputBytes, _ := os.ReadFile("biginput")
+	bigInputStrs := strings.Split(string(bigInputBytes), "\n")
+	bigInput := make([][]int, len(bigInputStrs))
+	for i, row := range bigInputStrs {
+		for _, numStr := range strings.Split(row[1:len(row)-1], ",") {
+			n, _ := strconv.Atoi(numStr)
+			bigInput[i] = append(bigInput[i], n)
+		}
+	}
+
 	for _, tc := range []struct {
 		grid [][]int
 		want int
 	}{
+		{bigInput, 18762},
 		{[][]int{
-			{1, 0, 1, 1, 0, 0, 0, 0, 0, 1},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 			{0, 1, 0, 0, 1, 1, 0, 1, 0, 0},
 			{0, 1, 1, 0, 0, 0, 0, 1, 0, 0},
 			{1, 1, 0, 1, 0, 0, 1, 0, 1, 0},
 			{0, 1, 1, 1, 0, 0, 0, 0, 1, 1},
 			{1, 0, 1, 1, 1, 1, 0, 0, 1, 0},
-		}, 10},
+		}, 9},
 		{[][]int{{0, 1, 0, 0}, {0, 1, 1, 0}, {1, 1, 1, 1}, {0, 1, 1, 0}, {0, 0, 1, 0}}, 4},
 		{[][]int{{0, 1, 0, 0}, {0, 1, 1, 0}, {1, 1, 1, 1}, {0, 1, 1, 0}, {0, 0, 1, 0}}, 4},
 		{[][]int{{1, 1, 1, 1, 1, 1, 1}}, 3},
@@ -34,163 +75,192 @@ func Test_minimumOperations(t *testing.T) {
 }
 
 func minimumOperations(grid [][]int) int {
+	m, n := len(grid), len(grid[0])
+	boolGrid := make([][]bool, m)
+	for i := range grid {
+		boolGrid[i] = make([]bool, n)
+		for j, val := range grid[i] {
+			boolGrid[i][j] = val == 1
+		}
+	}
+
 	// Each cell in the grid that contains a 1 can be traversed with BFS to form
 	// a bipartite graph.
-	//
 	var res int
 	for i := range grid {
 		for j := range grid[i] {
 			if grid[i][j] == 1 {
-				res += minCover(grid, i, j)
+				g := parseGraph(boolGrid, position{int16(i), int16(j)})
+				// Clear region
+				for _, n := range g.nodes {
+					grid[n.i][n.j] = 0
+				}
+
+				// Find minimum vertex cover / maximum matching (König's Theorem)
+				res += minCover(g)
 			}
 		}
 	}
 	return res
 }
 
-func minCover(grid [][]int, startI, startJ int) int {
-	// Parse bipartite graph
-	m, n := len(grid), len(grid[0])
-	adj := make([][]int, 1, 10)
-	adj[0] = []int{}
-	coords := [][]int{{startI, startJ}}
-	coordToIdx := make(map[[2]int]int)
-	coordToIdx[[2]int{startI, startJ}] = 0
-	grid[startI][startJ] = 0 // unset
-	cur := []int{0}
-	next := []int{}
-	deltas := [][]int{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
-	ok := func(i, j int) bool {
-		return i >= 0 && i < m && j >= 0 && j < n && grid[i][j] == 1
+type position struct {
+	i, j int16
+}
+
+func (p position) add(other position) position {
+	p.i += other.i
+	p.j += other.j
+	return p
+}
+
+type BipartiteGraph struct {
+	nodes []position
+	// leftAdj contains edges to the right side. Due to how Hopcraft-Karp is
+	// implemented (with a dummy node), it's not necessary to store right->left.
+	leftAdj [][]int
+	nLeft   int
+	nRight  int
+}
+
+// parseGraph parses the bipartite graph originating from (i,j)
+func parseGraph(grid [][]bool, start position) *BipartiteGraph {
+	g := new(BipartiteGraph)
+	m, n := int16(len(grid)), int16(len(grid[0]))
+
+	posToIdx := map[position]int{start: 0}
+	g.nodes = make([]position, 1, 100)
+	g.nodes[0] = start
+	cur := []position{start}
+	next := []position{}
+	var counts [2]int
+	counts[0]++
+	g.leftAdj = make([][]int, 1, 100)
+	g.leftAdj[0] = make([]int, 0, 4)
+
+	seen := map[position]struct{}{start: {}}
+	hasSeen := func(c position) bool {
+		_, exists := seen[c]
+		return exists
 	}
-	for len(cur) > 0 {
+	dirs := []position{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
+	ok := func(p position) bool {
+		return p.i >= 0 && p.i < m && p.j >= 0 && p.j < n && grid[p.i][p.j]
+	}
+
+	for level := 1; len(cur) > 0; level = (level + 1) % 2 {
 		next = next[:0]
-		for _, idx := range cur {
-			i, j := coords[idx][0], coords[idx][1]
-			for _, d := range deltas {
-				ii := i + d[0]
-				jj := j + d[1]
-				if !ok(ii, jj) {
+		for _, pos := range cur {
+			for _, dir := range dirs {
+				nei := pos.add(dir)
+				if !ok(nei) {
 					continue
 				}
-				neiIdx, exists := coordToIdx[[2]int{ii, jj}]
-				if !exists {
-					neiIdx = len(coords)
-					coordToIdx[[2]int{ii, jj}] = neiIdx
-					adj = append(adj, []int{})
-					next = append(next, len(coords))
-					coords = append(coords, []int{ii, jj})
+				if !hasSeen(nei) {
+					posToIdx[nei] = counts[level]
+					counts[level]++
+					g.nodes = append(g.nodes, nei)
+					seen[nei] = struct{}{}
+					next = append(next, nei)
+					if level == 0 {
+						g.leftAdj = append(g.leftAdj, make([]int, 0, 4))
+					}
 				}
-				adj[idx] = append(adj[idx], neiIdx)
-				adj[neiIdx] = append(adj[neiIdx], idx)
+				if level == 1 {
+					g.leftAdj[posToIdx[pos]] = append(g.leftAdj[posToIdx[pos]], posToIdx[nei])
+				}
 			}
 		}
 		cur, next = next, cur
 	}
+	g.nLeft = counts[0]
+	g.nRight = counts[1]
+	return g
+}
 
-	// Zero out block of coordinates
-	for _, c := range coords {
-		grid[c[0]][c[1]] = 0
-	}
-
-	nn := len(coords)
-	// matched[i] contains the coordinate index that i is matched to, or -1 if no
-	// matching currently exists
-	matching := make([]int, nn)
-	for i := range matching {
-		matching[i] = -1
-	}
-	if len(coords) == 1 {
-		return 0
-	}
-
-	// The minimum amount of changes made to cells with 1s is equal to the minimum
-	// vertex cover. According to König's theorem, the minimal vertex cover for
-	// an unweighted, bipartite graph is equal to the maximum cardinality matching.
-	//
-	// Therefore, we can use Hopcroft-Karp to find the maximum matching, which is
-	// equal to the min vertex cover.
-	//
-
-	// findPath finds an augmenting path (if any) and returns the length of the
-	// path. If no path was found, -1 is returned.
-	var findPath func(int, int) int
-	buf := make([]int, len(coords))
-	seen := make([]bool, len(coords))
-	findPath = func(cur, bufIdx int) (len int) {
-		if seen[cur] {
-			return -1
+func minCover(g *BipartiteGraph) int {
+	adj := g.leftAdj
+	dummyNode := 0
+	pairLeft := make([]int, g.nLeft+1)
+	pairV := make([]int, g.nRight+1)
+	dist := make([]int, g.nLeft+1)
+	// Due to dummy node, increment node indices by 1
+	adj = append(adj, []int{})
+	for i := len(adj) - 2; i >= 0; i-- {
+		for j := range adj[i] {
+			adj[i][j]++
 		}
-		seen[cur] = true
-		defer func() {
-			seen[cur] = false
-		}()
-		buf[bufIdx] = cur
-		if free := matching[cur] == -1; free && bufIdx > 0 {
-			return bufIdx + 1
-		}
-		shouldMatch := bufIdx%2 == 1
-		if shouldMatch {
-			if x := findPath(matching[cur], bufIdx+1); x != -1 {
-				return x
+		adj[i+1] = adj[i]
+	}
+	adj[0] = []int{}
+
+	const infty = math.MaxInt32
+
+	// BFS returns true if there exists at least one augmenting path which
+	// increases the total number of matchings. The distance of such a path is
+	// stored in the dummy node (dist[0])
+	bfs := func() bool {
+		// Initialize cur with any un-matched vertex on the left side
+		cur := []int{}
+		for u := 1; u <= g.nLeft; u++ {
+			if pairLeft[u] == dummyNode {
+				dist[u] = 0
+				cur = append(cur, u)
+			} else {
+				dist[u] = infty
 			}
-		} else {
-			// Find unmatched edges
-			for _, nei := range adj[cur] {
-				if nei == matching[cur] {
+		}
+		dist[dummyNode] = infty
+		next := []int{}
+		for len(cur) > 0 {
+			next = next[:0]
+			for _, u := range cur {
+				if dist[u] >= dist[dummyNode] {
 					continue
 				}
-				if x := findPath(nei, bufIdx+1); x != -1 {
-					return x
+				for _, v := range adj[u] {
+					if dist[pairV[v]] != infty {
+						continue
+					}
+					dist[pairV[v]] = dist[u] + 1
+					next = append(next, pairV[v])
 				}
 			}
+			cur, next = next, cur
 		}
-		return -1
+		return dist[dummyNode] != infty
 	}
 
-	// augment updates the matching
-	augment := func(path []int) {
-		for i := 0; i < len(path); i += 2 {
-			from, to := path[i], path[i+1]
-			// Clear existing matching
-			if matching[from] != -1 {
-				matching[from], matching[matching[from]] = -1, -1
-			}
-			if matching[to] != -1 {
-				matching[to], matching[matching[to]] = -1, -1
-			}
-			// Perform matching
-			matching[from], matching[to] = to, from
+	// DFS returns true if an augmenting path exists such that it starts from u.
+	// If such a path is found, the augmentation will be performed.
+	var dfs func(u int) bool
+	dfs = func(u int) bool {
+		if u == dummyNode {
+			return true
 		}
-	}
-
-	foundPath := true
-	for foundPath {
-		foundPath = false
-		// For each unmatched node
-		for nodeIdx, matchIdx := range matching {
-			if matchIdx != -1 {
+		for _, v := range adj[u] {
+			if dist[pairV[v]] != dist[u]+1 || !dfs(pairV[v]) {
 				continue
 			}
-			// Try to find an augmenting path
-			// If such a path is found, break and continue the loop
-			pathLen := findPath(nodeIdx, 0)
-			if pathLen <= 0 {
+			pairLeft[u] = v
+			pairV[v] = u
+			return true
+		}
+		dist[u] = infty
+		return false
+	}
+
+	var matching int
+	for bfs() {
+		for u := 1; u <= g.nLeft; u++ {
+			if pairLeft[u] != dummyNode {
 				continue
 			}
-			foundPath = true
-			path := buf[:pathLen]
-			augment(path)
-			break
+			if dfs(u) {
+				matching++
+			}
 		}
 	}
 
-	// Count matched vertices
-	var res int
-	for _, matchIdx := range matching {
-		if matchIdx != -1 {
-			res++
-		}
-	}
-	return res / 2
+	return matching
 }
