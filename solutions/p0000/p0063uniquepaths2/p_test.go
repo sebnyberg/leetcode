34eae4b12dc562c1bbfc867628f5014338cd843c
Sebastian Nyberg <seb.nyberg90@gmail.com>
2022-05-20 22:12:25 +0200

    Improve daily

--- solutions/p0000/p0063uniquepaths2/p_test.go
+++ solutions/p0000/p0063uniquepaths2/p_test.go
@@ -21,38 +21,19 @@ func Test_uniquePaths(t *testing.T) {
 }
 
 func uniquePathsWithObstacles(obstacleGrid [][]int) int {
-	// DP-solution
-	// Since the robot can only move down or right, the number of
-	// ways to reach the top and left cells in the grid is one
-	m := len(obstacleGrid)
-	n := len(obstacleGrid[0])
-	grid := make([][]int, m)
-	for i := range grid {
-		grid[i] = make([]int, n)
+	m, n := len(obstacleGrid), len(obstacleGrid[0])
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
 	}
-	for i := range obstacleGrid {
-		if obstacleGrid[i][0] == 1 {
-			break
-		}
-		grid[i][0] = 1
-	}
-	for i := range obstacleGrid[0] {
-		if obstacleGrid[0][i] == 1 {
-			break
-		}
-		grid[0][i] = 1
-	}
-
-	// The number of ways the robot can reach any other point in the
-	// grid is the sum of the ways it can reach the position above,
-	// and to the left
-	for i := 1; i < m; i++ {
-		for j := 1; j < n; j++ {
-			if obstacleGrid[i][j] == 1 {
+	dp[0][1] = 1
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if obstacleGrid[i-1][j-1] == 1 {
 				continue
 			}
-			grid[i][j] = grid[i-1][j] + grid[i][j-1]
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
 		}
 	}
-	return grid[m-1][n-1]
+	return dp[m][n]
 }
