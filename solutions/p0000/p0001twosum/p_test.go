f785fd233529fb12411cb5835ced93c4c71f4b0f
Sebastian Nyberg <seb.nyberg90@gmail.com>
2022-07-10 18:44:24 +0200

    Update old overengineered solutions

--- solutions/p0000/p0001twosum/p_test.go
+++ solutions/p0000/p0001twosum/p_test.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_twoSum(t *testing.T) {
@@ -19,21 +19,18 @@ func Test_twoSum(t *testing.T) {
 	}
 	for _, tc := range tcs {
 		t.Run(fmt.Sprintf("%v", tc.nums), func(t *testing.T) {
-			ij := twoSum(tc.nums, tc.target)
-			i, j := ij[0], ij[1]
-			assert.Equal(t, tc.want[0], i, "i")
-			assert.Equal(t, tc.want[1], j, "j")
+			require.Equal(t, tc.want, twoSum(tc.nums, tc.target))
 		})
 	}
 }
 
 func twoSum(nums []int, target int) []int {
+	numIdx := make(map[int]int, len(nums))
 	for i, n1 := range nums {
-		for j, n2 := range nums[i+1:] {
-			if n1+n2 == target {
-				return []int{i, j + i + 1}
-			}
+		if j, exists := numIdx[target-n1]; exists {
+			return []int{j, i}
 		}
+		numIdx[n1] = i
 	}
-	return []int{0, 0}
+	return nil
 }
--- solutions/p0000/p0002addtwonumbers/p_test.go
+++ solutions/p0000/p0002addtwonumbers/p_test.go
@@ -2,6 +2,7 @@ package p0002addtwonumbers
 
 import (
 	"fmt"
+	"log"
 	"strconv"
 	"strings"
 	"testing"
@@ -14,98 +15,92 @@ type ListNode struct {
 	Next *ListNode
 }
 
+func ParseList(s string) *ListNode {
+	s = strings.TrimPrefix(s, "[")
+	s = strings.TrimSuffix(s, "]")
+	items := strings.Split(s, ",")
+	dummy := &ListNode{}
+	cur := dummy
+	for _, item := range items {
+		num, err := strconv.Atoi(item)
+		if err != nil {
+			log.Fatalf("failed to convert string %v to integer", num)
+		}
+		cur.Next = &ListNode{
+			Val: num,
+		}
+		cur = cur.Next
+	}
+	return dummy.Next
+}
+
+func (n *ListNode) String() string {
+	cur := n
+	var sb strings.Builder
+	sb.WriteRune('[')
+	i := 0
+	for cur != nil {
+		if i != 0 {
+			sb.WriteRune(',')
+		}
+		sb.WriteString(strconv.Itoa(cur.Val))
+		i++
+		cur = cur.Next
+	}
+	sb.WriteRune(']')
+	return sb.String()
+}
+
+func (n *ListNode) Equals(other *ListNode) bool {
+	if n == nil {
+		return other == nil
+	}
+	if other == nil {
+		return false
+	}
+	return n.Val == other.Val && n.Next.Equals(other.Next)
+}
+
 func Test_addTwoNumbers(t *testing.T) {
 	tcs := []struct {
-		l1   []int
-		l2   []int
-		want []int
+		l1   string
+		l2   string
+		want string
 	}{
-		{[]int{2, 4, 3}, []int{5, 6, 4}, []int{7, 0, 8}},
-		{[]int{0}, []int{0}, []int{0}},
-		{[]int{9, 9, 9, 9, 9, 9, 9}, []int{9, 9, 9, 9}, []int{8, 9, 9, 9, 0, 0, 0, 1}},
+		{"[2,4,3]", "[5,6,4]", "[7,0,8]"},
 	}
 	for _, tc := range tcs {
 		t.Run(fmt.Sprintf("%v,%v", tc.l1, tc.l2), func(t *testing.T) {
-			l1 := createlistNode(tc.l1)
-			l2 := createlistNode(tc.l2)
-			require.EqualValues(t, tc.want, addTwoNumbers(l1, l2).Ints())
+			res := addTwoNumbers(ParseList(tc.l1), ParseList(tc.l2))
+			require.True(t, ParseList(tc.want).Equals(res))
 		})
 	}
 }
 
-// v2 re-uses l1 to generate output
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	res := l1
-
-	// Add l2 values to res
-	var nilNode = &ListNode{Val: 0}
+	dummy := &ListNode{}
+	val := func(l *ListNode) int {
+		if l != nil {
+			return l.Val
+		}
+		return 0
+	}
+	next := func(l *ListNode) *ListNode {
+		if l != nil {
+			return l.Next
+		}
+		return l
+	}
+	curr := dummy
 	var carry int
-	var sum int
-
-	for {
-		sum = l1.Val + l2.Val + carry
-		carry = sum / 10
-		l1.Val = sum % 10
-
-		if l1.Next == nil && l2.Next == nil {
-			if carry == 1 {
-				l1.Next = &ListNode{Val: carry}
-			}
-			break
-		}
-
-		// When there are no more values in l2, refer to a nilNode
-		if l2.Next == nil {
-			l2 = nilNode
-		} else {
-			l2 = l2.Next
-		}
-
-		if l1.Next == nil {
-			l1.Next = &ListNode{Val: 0}
-		}
-		l1 = l1.Next
+	for l1 != nil || l2 != nil || carry > 0 {
+		curr.Next = &ListNode{}
+		curr = curr.Next
+		curr.Val = val(l1) + val(l2) + carry
+		carry = curr.Val / 10
+		curr.Val %= 10
+		l1 = next(l1)
+		l2 = next(l2)
 	}
-	return res
-}
-
-func (l *ListNode) Ints() []int {
-	res := make([]int, 1)
-	res[0] = l.Val
-	for cur := l; cur.Next != nil; cur = cur.Next {
-		res = append(res, cur.Next.Val)
-	}
-	return res
-}
-
-func (l *ListNode) String() string {
-	var sb strings.Builder
-	sb.WriteString("[")
-	cur := l
-	for cur.Next != nil {
-		sb.WriteString(strconv.Itoa(cur.Val) + ",")
-		cur = cur.Next
-	}
-	sb.WriteString(strconv.Itoa(cur.Val))
-	sb.WriteString("]")
-	return sb.String()
-}
-
-func createlistNode(vals []int) *ListNode {
-	first := &ListNode{
-		Val: vals[0],
-	}
-	cur := first
-	for i := 1; i < len(vals); i++ {
-		next := ListNode{
-			Val: vals[i],
-		}
-		if i == 1 {
-			first.Next = &next
-		} else {
-			cur.Next = &next
-		}
-		cur = &next
-	}
-	return first
+	return dummy.Next
 }
