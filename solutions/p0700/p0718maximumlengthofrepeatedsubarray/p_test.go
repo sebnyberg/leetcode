23db3565e8d7ea7be424deb075fd93944d81d96e
Sebastian Nyberg <seb.nyberg90@gmail.com>
2022-09-20 10:26:59 +0200

    Use rolling has for p718

--- solutions/p0700/p0718maximumlengthofrepeatedsubarray/p_test.go
+++ solutions/p0700/p0718maximumlengthofrepeatedsubarray/p_test.go
@@ -23,26 +23,99 @@ func Test_findLength(t *testing.T) {
 }
 
 func findLength(nums1 []int, nums2 []int) int {
-	// This exercise is not a typical sub-array problem but rather a sequence
-	// matching problem. Typically sequence matching (e.g. edit distance) is done
-	// with either rolling hash or dynamic programming.
+	// Use binary search to find the smallest subarray length that is not
+	// contained in either integer arrays. The answer is one step smaller.
 	//
-	n, m := len(nums1), len(nums2)
-	dp := make([][]int, n+1)
-	for i := range dp {
-		dp[i] = make([]int, m+1)
+	// Rolling hash is used to hash windows over both num arrays.
+	// hashIndices stores indices of hashes found in nums1
+	// checking indices is required due to risk of collision
+	hashIndices := make(map[uint64][]int)
+
+	// mod should allow multiplication with fac without overflowing the hash
+	// container (in our case uint64).
+	// Bounds check:
+	// (1<<57) * 101 < (1<<57) * 128 = (1<<57)*(1<<7) = 1<<64
+	// https://primes.utm.edu/lists/2small/0bit.html
+	const mod = 1<<57 - 13
+	const fac = 101 // first prime larger than maximum possible value in nums
+
+	// checkAtIndex returns true if nums1[i:i+width] == nums2[j:j+width]
+	checkAtIndex := func(i, j int, width int) bool {
+		for k := 0; k < width; k++ {
+			if nums1[i+k] != nums2[j+k] {
+				return false
+			}
+		}
+		return true
 	}
-	var maxVal int
-	for i, n := range nums1 {
-		for j, m := range nums2 {
-			if n == m {
-				val := dp[i][j] + 1
-				dp[i+1][j+1] = val
-				if val > maxVal {
-					maxVal = val
+
+	n1, n2 := len(nums1), len(nums2)
+
+	// check checks whether there exists a subarray of length width that contains
+	// the same sequence of elements in both nums1 and nums2
+	check := func(width int) bool {
+		if width > n1 || width > n2 {
+			return false
+		}
+		// The base is the multiplication factor applied to the eldest number in the
+		// window. We use it to remove the eldest value from the hash.
+		var base uint64 = 1
+		for k := range hashIndices {
+			delete(hashIndices, k)
+		}
+		var h uint64
+		for i, x := range nums1 {
+			if i < width {
+				base = (base * fac) % mod
+			}
+			h = (h*fac + uint64(x)) % mod
+			if i < width-1 {
+				continue
+			}
+			if i >= width {
+				// Remove eldest number
+				h = (h - (uint64(nums1[i-width]) * base % mod) + mod) % mod
+			}
+			hashIndices[h] = append(hashIndices[h], i-width+1)
+		}
+		h = 0
+		for j, x := range nums2 {
+			h = (h*fac + uint64(x)) % mod
+			if j < width-1 {
+				continue
+			}
+			if j >= width {
+				// Remove eldest number
+				h = (h - (uint64(nums2[j-width]) * base % mod) + mod) % mod
+			}
+			// Check whether any indices in nums1 matches the subarray here.
+			// This is required since there may be hash collisions
+			if indices, exists := hashIndices[h]; exists {
+				for _, i := range indices {
+					if checkAtIndex(i, j-width+1, width) {
+						return true
+					}
 				}
 			}
 		}
+		return false
 	}
-	return maxVal
+
+	lo, hi := 0, min(n1, n2)+1
+	for lo < hi {
+		mid := lo + (hi-lo)/2
+		if check(mid) {
+			lo = mid + 1
+		} else {
+			hi = mid
+		}
+	}
+	return hi - 1
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
