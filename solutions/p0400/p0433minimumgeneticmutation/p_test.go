5da7c9987fb7c68205ef965870a419d9948e098f
Sebastian Nyberg <seb.nyberg90@gmail.com>
2022-11-02 08:48:52 +0100

    Update p0433 with cleaner solution

--- solutions/p0400/p0433minimumgeneticmutation/p_test.go
+++ solutions/p0400/p0433minimumgeneticmutation/p_test.go
@@ -26,70 +26,35 @@ func Test_minMutation(t *testing.T) {
 }
 
 func minMutation(start string, end string, bank []string) int {
-	// A mutation can be considered a replacement of a character such that the
-	// result belongs in the bank.
-
-	// For each gene, create "mappers" where one letter has been removed from the
-	// gene. If two genes could end on the same mapper, then they must be
-	// compatible.
-	findMappers := func(s string) []string {
-		bs := []byte(s)
-		res := make([]string, 0, len(s)-1)
-		for i := range bs {
-			res = append(res, string(bs[:i])+string('*')+string(bs[i+1:]))
-		}
-		return res
-	}
-
-	// End must be in the gene bank or there is no solution to this problem
-	var hasEnd bool
-	for _, gene := range bank {
-		if gene == end {
-			hasEnd = true
-			break
-		}
-	}
-	if !hasEnd {
-		return -1
-	}
-
-	mapperGenes := make(map[string][]string)
-	for _, gene := range bank {
-		if gene == end || gene == start { // don't add start / end twice..
-			continue
-		}
-		for _, m := range findMappers(gene) {
-			mapperGenes[m] = append(mapperGenes[m], gene)
-		}
-	}
-	for _, m := range findMappers(end) {
-		mapperGenes[m] = append(mapperGenes[m], end)
-	}
-
-	seen := make(map[string]struct{})
-	seen[start] = struct{}{}
-	cur := []string{start}
-	next := []string{}
-	var mutations int
-	for len(cur) > 0 {
-		mutations++
-		next = next[:0]
-		for _, s := range cur {
-			// Find possible mutations
-			for _, mapper := range findMappers(s) {
-				for _, gene := range mapperGenes[mapper] {
-					if _, exists := seen[gene]; exists {
-						continue
-					}
-					seen[gene] = struct{}{}
-					if gene == end {
-						return mutations
-					}
-					next = append(next, gene)
-				}
+	dist := func(a, b string) int {
+		var d int
+		for i := range a {
+			if a[i] != b[i] {
+				d++
 			}
 		}
-		cur, next = next, cur
+		return d
+	}
+	curr := []string{start}
+	next := []string{}
+	seen := make(map[string]struct{}, len(bank))
+	seen[start] = struct{}{}
+	for steps := 1; len(curr) > 0; steps++ {
+		next = next[:0]
+		for _, a := range curr {
+			for _, b := range bank {
+				_, exists := seen[b]
+				if exists || dist(a, b) > 1 {
+					continue
+				}
+				if b == end {
+					return steps
+				}
+				seen[b] = struct{}{}
+				next = append(next, b)
+			}
+		}
+		curr, next = next, curr
 	}
 	return -1
 }
