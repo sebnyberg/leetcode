489f29b038eb7d2789f125d99611d0d799ca2f5b
Sebastian Nyberg <seb.nyberg90@gmail.com>
2022-11-04 10:58:10 +0100

    Semi-branchless reverse vowels

--- solutions/p0300/p0345reversevowelsofastring/p_test.go
+++ solutions/p0300/p0345reversevowelsofastring/p_test.go
@@ -12,9 +12,9 @@ func Test_reverseVowels(t *testing.T) {
 		s    string
 		want string
 	}{
-		{"aA", "Aa"},
 		{"hello", "holle"},
 		{"leetcode", "leotcede"},
+		{"aA", "Aa"},
 	} {
 		t.Run(fmt.Sprintf("%+v", tc.s), func(t *testing.T) {
 			require.Equal(t, tc.want, reverseVowels(tc.s))
@@ -22,27 +22,27 @@ func Test_reverseVowels(t *testing.T) {
 	}
 }
 
+var vowel = [256]uint8{
+	'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1,
+	'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1,
+}
+var swap = [256][256]uint8{
+	'a': vowel, 'e': vowel, 'i': vowel, 'o': vowel, 'u': vowel,
+	'A': vowel, 'E': vowel, 'I': vowel, 'O': vowel, 'U': vowel,
+}
+
 func reverseVowels(s string) string {
-	var l, r int = 0, len(s) - 1
-	var vowel [256]bool
-	for _, ch := range []byte{
-		'a', 'e', 'i', 'o', 'u',
-		'A', 'E', 'I', 'O', 'U',
-	} {
-		vowel[ch] = true
-	}
 	bs := []byte(s)
+	var l, r int = 0, len(s) - 1
 	for l < r {
-		switch {
-		case !vowel[s[l]]:
-			l++
-		case !vowel[s[r]]:
-			r--
-		default:
-			bs[l], bs[r] = bs[r], bs[l]
-			l++
-			r--
-		}
+		a, b := s[l], s[r]
+		targetL := l + int(swap[a][b])*(r-l)
+		targetR := r - int(swap[a][b])*(r-l)
+		bs[targetL], bs[targetR] = bs[l], bs[r]
+		moveR := ^(^vowel[a] & vowel[b]) & 1
+		moveL := ^(vowel[a] & ^vowel[b]) & 1
+		l += int(moveL)
+		r -= int(moveR)
 	}
 	return string(bs)
 }
