0857c44f89f2be4e813057437157cd88e951096b
Sebastian Nyberg <seb.nyberg90@gmail.com>
2022-11-04 09:02:06 +0100

    Update daily with cleaner solution

--- solutions/p0300/p0345reversevowelsofastring/p_test.go
+++ solutions/p0300/p0345reversevowelsofastring/p_test.go
@@ -23,23 +23,22 @@ func Test_reverseVowels(t *testing.T) {
 }
 
 func reverseVowels(s string) string {
-	vowels := map[byte]bool{
-		'a': true, 'A': true,
-		'e': true, 'E': true,
-		'i': true, 'I': true,
-		'o': true, 'O': true,
-		'u': true, 'U': true,
+	var l, r int = 0, len(s) - 1
+	var vowel [256]bool
+	for _, ch := range []byte{
+		'a', 'e', 'i', 'o', 'u',
+		'A', 'E', 'I', 'O', 'U',
+	} {
+		vowel[ch] = true
 	}
 	bs := []byte(s)
-	l, r := 0, len(s)-1
 	for l < r {
-		for l <= r && !vowels[bs[l]] {
+		switch {
+		case !vowel[s[l]]:
 			l++
-		}
-		for l <= r && !vowels[bs[r]] {
+		case !vowel[s[r]]:
 			r--
-		}
-		if l < r {
+		default:
 			bs[l], bs[r] = bs[r], bs[l]
 			l++
 			r--
