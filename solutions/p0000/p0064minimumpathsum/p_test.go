68552f587d41f23b2d22264ac684bfbf81c9d2a9
Sebastian Nyberg <seb.nyberg90@gmail.com>
2023-03-27 09:41:20 +0200

    p0064minimumpathsum: O(n) DP

--- solutions/p0000/p0064minimumpathsum/p_test.go
+++ solutions/p0000/p0064minimumpathsum/p_test.go
@@ -2,6 +2,7 @@ package p0064minimumpathsum
 
 import (
 	"fmt"
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -21,32 +22,26 @@ func Test_minPathSum(t *testing.T) {
 	}
 }
 
+func minPathSum(grid [][]int) int {
+	m, n := len(grid), len(grid[0])
+	prev := make([]int, n+1)
+	curr := make([]int, n+1)
+	for j, v := range grid[0] {
+		prev[j+1] = prev[j] + v
+	}
+	for i := 1; i < m; i++ {
+		curr[0] = math.MaxInt32
+		for j := 0; j < n; j++ {
+			curr[j+1] = grid[i][j] + min(curr[j], prev[j+1])
+		}
+		prev, curr = curr, prev
+	}
+	return prev[n]
+}
+
 func min(a, b int) int {
 	if a < b {
 		return a
 	}
 	return b
-}
-
-func minPathSum(grid [][]int) int {
-	m, n := len(grid), len(grid[0])
-	sum := make([][]int, m)
-	for i := range sum {
-		sum[i] = make([]int, n)
-		if i == 0 {
-			sum[i][0] = grid[i][0]
-			continue
-		}
-		sum[i][0] = sum[i-1][0] + grid[i][0]
-	}
-	for i := 1; i < n; i++ {
-		sum[0][i] = sum[0][i-1] + grid[0][i]
-	}
-
-	for i := 1; i < m; i++ {
-		for j := 1; j < n; j++ {
-			sum[i][j] = min(sum[i-1][j], sum[i][j-1]) + grid[i][j]
-		}
-	}
-	return sum[m-1][n-1]
 }
