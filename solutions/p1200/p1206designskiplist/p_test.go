f849c97dc291eb8b558fce3ee69b73f1942b4a8c
Sebastian Nyberg <seb.nyberg90@gmail.com>
2023-04-21 14:10:29 +0200

    p1206designskiplist

--- solutions/p1200/p1206designskiplist/p_test.go
+++ solutions/p1200/p1206designskiplist/p_test.go
@@ -1,7 +1,6 @@
 package p1206designskiplist
 
 import (
-	"math"
 	"math/rand"
 	"testing"
 
@@ -15,67 +14,109 @@ func TestSkiplist(t *testing.T) {
 	a.Add(3)
 	res := a.Search(0)
 	require.False(t, res)
+	res = a.Search(1)
+	require.True(t, res)
+	res = a.Search(2)
+	require.True(t, res)
+	res = a.Search(3)
+	require.True(t, res)
 }
 
-const layers = 4
+const (
+	maxLevel = 32
+)
 
 type node struct {
 	val  int
-	next [layers]*node
+	next []*node
 }
 
 type Skiplist struct {
-	dummy *node
+	head  *node
+	level int
 }
 
 func Constructor() Skiplist {
+	head := &node{
+		next: make([]*node, maxLevel),
+	}
 	return Skiplist{
-		dummy: &node{val: -1},
+		head: head,
 	}
 }
 
-func next(n *node) int {
-	if n != nil {
-		return n.val
-	}
-	return math.MaxInt32
-}
-
-func (this *Skiplist) Search(target int) bool {
-	curr := this.dummy
-	for l := layers - 1; l >= 0; l-- {
-		for target > next(curr.next[l]) {
-			curr = curr.next[l]
+func (s *Skiplist) Search(target int) bool {
+	cur := s.head
+	for i := s.level - 1; i >= 0; i-- {
+		for cur.next[i] != nil && cur.next[i].val < target {
+			cur = cur.next[i]
 		}
 	}
-	return curr.val == target
+	return cur.next[0] != nil && cur.next[0].val == target
 }
 
-func (this *Skiplist) Add(num int) {
-	curr := this.dummy
-	// pre[i] is the node prior to the node that is being inserted on each level
-	pre := make([]*node, layers)
-	for l := layers - 1; l >= 0; l-- {
-		for num > next(curr.next[l]) {
-			curr = curr.next[l]
+func (s *Skiplist) Add(num int) {
+	// update contains a list of nodes that come prior to the new node to insert
+	update := make([]*node, maxLevel)
+	cur := s.head
+	for i := s.level - 1; i >= 0; i-- {
+		for cur.next[i] != nil && cur.next[i].val < num {
+			cur = cur.next[i]
 		}
-		pre[l] = curr
+		update[i] = cur
 	}
 	level := randomLevel()
-	node := &node{val: num}
+	if level > s.level {
+		for i := s.level; i < level; i++ {
+			update[i] = s.head
+		}
+		s.level = level
+	}
+	newNode := &node{
+		val:  num,
+		next: make([]*node, level),
+	}
 	for i := 0; i < level; i++ {
-		pre[i].next[i], node.next[i] = node, pre[i].next[i]
+		newNode.next[i] = update[i].next[i]
+		update[i].next[i] = newNode
 	}
 }
 
 func randomLevel() int {
 	level := 1
-	for rand.Intn(2) == 0 && level < layers {
+	for rand.Intn(2) == 1 && level < maxLevel {
 		level++
 	}
 	return level
 }
 
-func (this *Skiplist) Erase(num int) bool {
+func (s *Skiplist) Erase(num int) bool {
+	update := make([]*node, maxLevel)
+
+	cur := s.head
+	for i := s.level - 1; i >= 0; i-- {
+		for cur.next[i] != nil && cur.next[i].val < num {
+			cur = cur.next[i]
+		}
+		update[i] = cur
+	}
+
+	if cur.next[0] == nil || cur.next[0].val != num {
+		return false
+	}
+
+	// erase
+	nodeToRemove := cur.next[0]
+	for i := 0; i < s.level; i++ {
+		if update[i].next[i] != nodeToRemove {
+			// all levels with this node have already been removed
+			break
+		}
+		update[i].next[i] = nodeToRemove.next[i]
+	}
+	// prune empty levels (if any)
+	for s.level > 1 && s.head.next[s.level-1] == nil {
+		s.level--
+	}
 	return true
 }
