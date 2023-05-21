725ec1aa544908f96740fd3bd5efb97e234fd8ed
Sebastian Nyberg <seb.nyberg90@gmail.com>
2023-05-21 19:02:11 +0200

    p1325deleteleaveswithagivenvalue

--- solutions/p1300/p1325deleteleaveswithagivenvalue/p_test.go
+++ solutions/p1300/p1325deleteleaveswithagivenvalue/p_test.go
@@ -7,6 +7,13 @@ type TreeNode struct {
 }
 
 func removeLeafNodes(root *TreeNode, target int) *TreeNode {
+	if root == nil {
+		return nil
+	}
 	root.Left = removeLeafNodes(root.Left, target)
 	root.Right = removeLeafNodes(root.Right, target)
+	if root.Left == nil && root.Right == nil && root.Val == target {
+		return nil
+	}
+	return root
 }
