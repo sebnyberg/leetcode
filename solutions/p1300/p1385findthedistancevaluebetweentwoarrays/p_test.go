28a4df89ad97d6909b9a2c9630b72ee4c6f2d72c
Sebastian Nyberg <seb.nyberg90@gmail.com>
2023-06-10 01:01:34 +0200

    p1385findthedistancevaluebetweentwoarrays

---
+++ solutions/p1300/p1385findthedistancevaluebetweentwoarrays/p_test.go
@@ -0,0 +1,40 @@
+package p1385findthedistancevaluebetweentwoarrays
+
+import "sort"
+
+func findTheDistanceValue(arr1 []int, arr2 []int, d int) int {
+	sort.Ints(arr1)
+	sort.Ints(arr2)
+
+	var res int
+	for _, x := range arr1 {
+		i := sort.SearchInts(arr2, x)
+		pre := arr2[0]
+		post := arr2[0]
+		if i < len(arr2) {
+			pre = arr2[i]
+		}
+		if i < len(arr2)-1 {
+			post = arr2[i+1]
+		}
+		if min(abs(x-pre), abs(post-x)) <= d {
+			continue
+		}
+		res++
+	}
+	return res
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
