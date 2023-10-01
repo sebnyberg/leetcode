0d5f7b8e61f915b69214d78fb8c14836025611a2
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2023-10-01 18:12:50 +0200

    p2619arrayprototypelast

---
+++ solutions/p2600/p2619arrayprototypelast/p.ts
@@ -0,0 +1,19 @@
+declare global {
+  interface Array<T> {
+    last(): T | -1;
+  }
+}
+
+Array.prototype.last = function () {
+  if (this?.length != 0) {
+    return this[this.length - 1];
+  }
+  return -1;
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
+
+export {};
