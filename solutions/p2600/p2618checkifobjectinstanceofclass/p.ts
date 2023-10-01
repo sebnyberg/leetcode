de955435f204f43cdba05f6a9d433bbf0b242cf2
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2023-10-01 18:11:09 +0200

    p2618checkifobjectinstanceofclass

---
+++ solutions/p2600/p2618checkifobjectinstanceofclass/p.ts
@@ -0,0 +1,13 @@
+function checkIfInstanceOf(obj: any, classFunction: any): boolean {
+  while (obj != null) {
+    if (obj.constructor === classFunction) {
+      return true;
+    }
+    obj = Object.getPrototypeOf(obj);
+  }
+  return false;
+}
+
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */
