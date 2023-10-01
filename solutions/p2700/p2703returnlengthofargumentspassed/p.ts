f43336a97c81f0135513828c2da2a3a2cbc4fb1e
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2023-10-01 18:11:16 +0200

    p2703returnlengthofargumentspassed

---
+++ solutions/p2700/p2703returnlengthofargumentspassed/p.ts
@@ -0,0 +1,15 @@
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+
+function argumentsLength(...args: JSONValue[]): number {
+  return args.length;
+}
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
