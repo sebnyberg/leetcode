e0f1343a0a4f00529987f4047704992ea399fee0
Sebastian Nyberg <sebastian.nyberg@sentian.ai>
2023-10-29 21:59:24 +0100

    log -> log2

--- solutions/p0400/p0458poorpigs/p_test.go
+++ solutions/p0400/p0458poorpigs/p_test.go
@@ -57,5 +57,5 @@ func poorPigs(buckets int, minutesToDie int, minutesToTest int) int {
 	// x >= ln(buckets) / ln(experiments+1)
 	// x = ceil(ln(buckets) / ln(experiments+1))
 	experiments := minutesToTest / minutesToDie
-	return int(math.Ceil(math.Log(float64(buckets)) / math.Log(float64(experiments+1))))
+	return int(math.Ceil(math.Log2(float64(buckets)) / math.Log2(float64(experiments+1))))
 }
