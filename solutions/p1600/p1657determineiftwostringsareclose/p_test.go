41a27239156ee34e72ccc40b7c774f45a94a6f5e
Sebastian Nyberg <sebastian.nyberg@mail.weir>
2024-01-14 21:00:48 +0100

    p1657determineiftwostringsareclose

---
+++ solutions/p1600/p1657determineiftwostringsareclose/p_test.go
@@ -0,0 +1,30 @@
+package p1657determineiftwostringsareclose
+
+import (
+	"fmt"
+)
+
+func closeStrings(word1 string, word2 string) bool {
+	doCount := func(a string) ([]int, int) {
+		var bm int
+		res := make([]int, 26)
+		for _, ch := range a {
+			res[ch-'a']++
+			bm |= (1 << (ch - 'a'))
+		}
+		return res, bm
+	}
+	a, ba := doCount(word1)
+	b, bb := doCount(word2)
+	if ba != bb {
+		return false
+	}
+	sort.Ints(a)
+	sort.Ints(b)
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
