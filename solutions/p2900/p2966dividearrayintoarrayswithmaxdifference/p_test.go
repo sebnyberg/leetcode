13521ae5076dbf8a33256c78c0097a63a32e870d
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2024-02-11 11:48:51 +0100

    p2966dividearrayintoarrayswithmaxdifference

--- solutions/p2900/p2966dividearrayintoarrayswithmaxdifference/p_test.go
+++ solutions/p2900/p2966dividearrayintoarrayswithmaxdifference/p_test.go
@@ -1,21 +1,18 @@
-package main
+package p2966dividearrayintoarrayswithmaxdifference
 
-import (
-   "fmt"
-)
-
+import "sort"
 
 func divideArray(nums []int, k int) [][]int {
-    sort.Ints(nums)
-    if len(nums) % 3 != 0 {
-        return nil
-    }
-    var res [][]int
-    for i := 0; i < len(nums); i += 3 {
-        if nums[i+2] - nums[i] > k {
-            return nil
-        }
-        res = append(res, nums[i:i+3])
-    }
-    return res
+	sort.Ints(nums)
+	if len(nums)%3 != 0 {
+		return nil
+	}
+	var res [][]int
+	for i := 0; i < len(nums); i += 3 {
+		if nums[i+2]-nums[i] > k {
+			return nil
+		}
+		res = append(res, nums[i:i+3])
+	}
+	return res
 }
