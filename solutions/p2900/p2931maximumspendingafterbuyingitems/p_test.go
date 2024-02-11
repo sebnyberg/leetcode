431fd19de4555533e165b8fe47d9f39f51f656bf
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2024-02-11 18:19:13 +0100

    p2931maximumspendingafterbuyingitems

---
+++ solutions/p2900/p2931maximumspendingafterbuyingitems/p_test.go
@@ -0,0 +1,17 @@
+package p2931maximumspendingafterbuyingitems
+
+func maxSpending(values [][]int) int64 {
+	// Hm, isn't this just a matter of buying products from cheap to expensive?
+	// We could just sort all values...
+	var concat []int
+	for _, v := range values {
+		concat = append(concat, v...)
+	}
+	sort.Ints(concat)
+	var res int
+	for i := range concat {
+		res += (i + 1) * concat[i]
+	}
+
+	return int64(res)
+}
