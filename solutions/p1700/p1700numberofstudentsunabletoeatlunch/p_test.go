af068d275db452cc08a2f0103d71f098189e9c78
Sebastian Nyberg <seb.nyberg90@gmail.com>
2024-04-09 00:32:50 +0100

    p1700numberofstudentsunabletoeatlunch

---
+++ solutions/p1700/p1700numberofstudentsunabletoeatlunch/p_test.go
@@ -0,0 +1,17 @@
+package p1700numberofstudentsunabletoeatlunch
+
+func countStudents(students []int, sandwiches []int) int {
+    var count [2]int
+    for _, s := range students {
+        count[s]++
+    }
+    res := len(sandwiches)
+    for _, s := range sandwiches {
+        if count[s] == 0 {
+            break
+        }
+        count[s]--
+        res--
+    }
+    return res
+}
