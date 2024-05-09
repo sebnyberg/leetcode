61595c7465a06fd30791294c2aa47ad9e7575ca9
Sebastian Nyberg <sebastian.nyberg@sentian.ai>
2024-05-09 08:44:51 +0200

    p3075

--- solutions/p3000/p3075maximizehappinessofselectedchildren/p_test.go
+++ solutions/p3000/p3075maximizehappinessofselectedchildren/p_test.go
@@ -1,1 +1,17 @@
 package p3075maximizehappinessofselectedchildren
+
+import "sort"
+
+func maximumHappinessSum(happiness []int, k int) int64 {
+	// Since the change in happiness is independent of the position of the
+	// children, the order of picked children is irrelevant. Therefore, we can
+	// just pick children in order of size and decrement by i each time until k ==
+	// 0 or the current happiness is 0.
+	sort.Sort(sort.Reverse(sort.IntSlice(happiness)))
+	var res int
+	for i := 0; i < len(happiness) && k > 0 && happiness[i]-i > 0; i++ {
+		res += happiness[i] - i
+		k--
+	}
+	return int64(res)
+}
