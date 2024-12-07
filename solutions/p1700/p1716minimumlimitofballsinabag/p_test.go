a7ce590dee9cb6c2abb0f823872b0684854b8aee
Sebastian Nyberg <seb.nyberg90@gmail.com>
2024-12-07 14:25:46 +0100

    Create p_test.go

---
+++ solutions/p1700/p1716minimumlimitofballsinabag/p_test.go
@@ -0,0 +1,26 @@
+package p1716minimumlimitofballsinabag
+
+func minimumSize(nums []int, maxOperations int) int {
+	// binary search
+	ok := func(y int) bool {
+		var res int
+		for _, x := range nums {
+			res += (x - 1) / y
+			if res > maxOperations {
+				return false
+			}
+		}
+		return true
+	}
+	lo := 1
+	hi := math.MaxInt32
+	for lo < hi {
+		mid := lo + (hi-lo)/2
+		if ok(mid) {
+			hi = mid
+		} else {
+			lo = mid + 1
+		}
+	}
+	return hi
+}
