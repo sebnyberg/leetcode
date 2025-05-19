fab566d26b5171e6d7d190bc7fb20384febfbb7b
Sebastian Nyberg <seb.nyberg90@gmail.com>
2025-05-19 11:58:00 +0200

    p3024

---
+++ solutions/p3000/p3024typeoftriangle/p_test.go
@@ -0,0 +1,18 @@
+package p3024typeoftriangle
+
+func triangleType(nums []int) string {
+    sort.Ints(nums)
+    a := nums[0]
+    b := nums[1]
+    c := nums[2]
+    switch {
+        case a + b <= c:
+            return "none"
+        case a == b && b == c:
+            return "equilateral"
+        case a == b || b == c:
+            return "isosceles"
+        default:
+            return "scalene"
+    }
+}
